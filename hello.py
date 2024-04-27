0b832829523274ccf98bce555156926b92a00ac9
agichimu <alexandergichimu@zetech.ac.ke>
2024-04-27 17:07:23 +0300

    date created comment

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 """
-python3 hello.py
+Created on Saturday 27 April 2024
+@author: Alexander Gichimu
 """
+
 # Ask users for their name
 name = input("Whats your name? \n ").strip().upper().split()
 # Name in this case is a variable
